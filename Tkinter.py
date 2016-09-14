a779eee03c4c3831f5d5ce6b4ec485f0a59d1312
1bjjohnson <bjjerald@gmail.com>
2016-09-13 20:48:01 -0600

    [Second commit having problems with import Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -1,8 +1,9 @@
-from Tkinter import *
+# from Tkinter import *
+import Tkinter as tk
 from tkFileDialog import *
 from PIL import ImageTk, Image
 
-root = Tk()
+root =
 root.wm_title("META tool")
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 # root.overrideredirect(1)
@@ -12,12 +13,10 @@ root.geometry("%dx%d+0+0" % (w, h))
 
 path = "photo.jpg"
 
-#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-img = ImageTk.PhotoImage(Image.open(path))
-#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+img = tk.ImageTk.PhotoImage(Image.open(path))
+
 panel = Label(root, image = img)
 
-#The Pack geometry manager packs widgets in rows or columns.
 panel.pack(side = "top", fill = "both", expand = "yes")
 
 # top = Toplevel()
